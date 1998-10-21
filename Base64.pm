1c67894df9c5265651b9ab22d33a7f7ccdc2b07b
Gisle Aas <gisle@aas.no>
1998-10-21 13:01:08 +0000

    2.07

--- Base64.pm
+++ Base64.pm
@@ -98,7 +98,7 @@ require DynaLoader;
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw(encode_base64 decode_base64);
 
-$VERSION = '2.06';
+$VERSION = '2.07';
 
 eval { bootstrap MIME::Base64 $VERSION; };
 if ($@) {
