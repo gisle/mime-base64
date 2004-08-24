ab17eaa5f694b2ede330009fac8a48bc179810ec
Gisle Aas <gisle@aas.no>
2004-08-24 16:22:27 +0000

    Doc tweaks.

--- QuotedPrint.pm
+++ QuotedPrint.pm
@@ -52,27 +52,27 @@ The following functions are provided:
 
 =item encode_qp($str, $eol, $binmode)
 
-This function returns an encoded version of the string given as
+This function returns an encoded version of the string ($str) given as
 argument.
 
-The second argument is the line-ending sequence to use.  It is
-optional and defaults to "\n".  Every occurrence of "\n" is
-replaced with this string, and it is also used for additional
-"soft line breaks" to ensure that no line is longer than 76
-characters.  You might want to pass it as "\015\012" to produce data
-suitable for external consumption.  The string "\r\n" produces the
-same result on many platforms, but not all.
+The second argument ($eol) is the line-ending sequence to use.  It is
+optional and defaults to "\n".  Every occurrence of "\n" is replaced
+with this string, and it is also used for additional "soft line
+breaks" to ensure that no line end up longer than 76 characters.  Pass
+it as "\015\012" to produce data suitable for external consumption.
+The string "\r\n" produces the same result on many platforms, but not
+all.
+
+The third argument ($binmode) will select binary mode if passed as a
+TRUE value.  In binary mode "\n" will be encoded in the same way as
+any other non-printable character.  This ensures that a decoder will
+end up with exactly the same string whatever line ending sequence it
+uses.  In general it is preferable to use the base64 encoding for
+binary data; see L<MIME::Base64>.
 
 An $eol of "" (the empty string) is special.  In this case, no "soft
-line breaks" are introduced and any "\n" in the original data is
-encoded as well.
-
-The third argument will select binary mode if passed as a TRUE value.
-In binary mode "\n" will be encoded in the same way as any other
-non-printable character.  This ensures that a decoder will end up with
-exactly the same string whatever line ending sequence it uses.  In
-general it is preferable to use the base64 encoding of binary data;
-see L<MIME::Base64>.
+line breaks" are introduced and binary mode is effectively enabled so
+that any "\n" in the original data is encoded as well.
 
 =item decode_qp($str);
 
