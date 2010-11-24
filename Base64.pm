33d7c69d6aa2224c4c10908053df35a7790136d3
Gisle Aas <gisle@aas.no>
2010-11-24 23:30:32 +0100

    Copyright 2010

--- Base64.pm
+++ Base64.pm
@@ -166,7 +166,7 @@ example:
 
 =head1 COPYRIGHT
 
-Copyright 1995-1999, 2001-2004 Gisle Aas.
+Copyright 1995-1999, 2001-2004, 2010 Gisle Aas.
 
 This library is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
