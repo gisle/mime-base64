50e5cfc58326e59630a09267407e41acfe19e141
Nicolas R <atoomic@cpan.org>
2017-11-02 17:07:59 -0500

    Avoid using 'vars' when 'our' works
    Anything after perl 5.6.0 will work fine with our
    which is recommended over using 'use var'.
    
    As mentioned by the vars doc https://perldoc.perl.org/vars.html
    usage of 'vars' pragma is discouraged and has been superseded
    by 'our' declarations available in Perl v5.6.0 or later.
    
    Additionally using 'vars' pragma increase the memory consumption
    of a program by about 700 kB for no good reason.
    
    Similar changes are about to be merged to CORE.
    
    References: RT-132077

--- QuotedPrint.pm
+++ QuotedPrint.pm
@@ -1,13 +1,12 @@
 package MIME::QuotedPrint;
 
 use strict;
-use vars qw(@ISA @EXPORT $VERSION);
 
 require Exporter;
-@ISA = qw(Exporter);
-@EXPORT = qw(encode_qp decode_qp);
+our @ISA = qw(Exporter);
+our @EXPORT = qw(encode_qp decode_qp);
 
-$VERSION = "3.13";
+our $VERSION = "3.13";
 
 use MIME::Base64;  # will load XS version of {en,de}code_qp()
 
